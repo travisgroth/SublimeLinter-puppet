1e4c1dee2218aede8f30f334f907fc785179cd40
stopdropandrew <andrew@kongregate.com>
2014-01-15 14:33:27 -0800

    Initial commit

---
+++ linter.py
@@ -0,0 +1,24 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Andrew Grim
+# Copyright (c) 2014 Andrew Grim
+#
+# License: MIT
+#
+
+"""This module exports the Puppet plugin class."""
+
+from SublimeLinter.lint import Linter, util
+
+
+class Puppet(Linter):
+
+    """Provides an interface to puppet."""
+
+    syntax = 'puppet'
+    cmd = ('puppet', 'parser', 'validate', '--color=false')
+    regex = r'^(?P<error>Error:).+?(?P<message>Syntax error at \'(?P<near>.+?)\'; expected \'.+\').+?line (?P<line>\d+)'
+    error_stream = util.STREAM_STDERR
+    defaults = {}
