3a4a67a34359c70ac9f3d0f19db3521f6bea7e48
Travis Groth <travis@netfoo.org>
2015-10-06 22:15:46 -0400

    Support Additional Error Output Formats
    Make the 'near' match group more flexible to support multiple error
    output styles for some syntax errors.
    
    Examples:
    
        Error: Could not parse for environment production: Syntax error at 'class' at line 27
        Error: Could not parse for environment production: Syntax error at end of file at line 32
        Error: Could not parse for environment production: Syntax error at ','; expected '}' at line 28
    
    See https://regex101.com/r/aT3aR3/3 for testing

--- linter.py
+++ linter.py
@@ -19,5 +19,5 @@ class Puppet(Linter):
 
     syntax = 'puppet'
     cmd = ('puppet', 'parser', 'validate', '--color=false')
-    regex = r'^Error:.+?(?P<message>Syntax error at \'(?P<near>.+?)\'; expected \'.+\').+?line (?P<line>\d+)'
+    regex = r'^Error:.+?(?P<message>Syntax error at \'?(?P<near>.+?)\'?(?:; expected \'.+\')?) at line (?P<line>\d+)'
     error_stream = util.STREAM_STDERR
